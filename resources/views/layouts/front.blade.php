83fd3ded61ba2342f6e8a6e46e71b1cd418eed71
capt3n <tangkasfx@gmail.com>
2022-12-27 17:48:28 +0700

    rename menu

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -19,7 +19,7 @@
             <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
                 <div class="container">
                     <a class="navbar-brand" href="/">
-                        CodeQ Blog
+                        Home
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                         <span class="navbar-toggler-icon"></span>
@@ -34,8 +34,8 @@
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
                             
-                            <li><a href="/blog" class="nav-link">Blog Page</a></li>
-                            <li><a href="/penulis" class="nav-link">Author List</a></li>
+                            <li><a href="/blog" class="nav-link">KodeQ Blog</a></li>
+                            <li><a href="/penulis/2" class="nav-link">Profil</a></li>
                             <!-- Authentication Links -->
                         @guest
 						<!--
