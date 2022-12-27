ea18ee557dd789d78f091d9dc072c1b3fd78c390
capt3n <tangkasfx@gmail.com>
2022-12-27 17:36:37 +0700

    membalikan akses top menu

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -37,7 +37,6 @@
                             <li><a href="/blog" class="nav-link">Blog Page</a></li>
                             <li><a href="/penulis" class="nav-link">Author List</a></li>
                             <!-- Authentication Links -->
-						<!--
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -47,21 +46,10 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
-						
                         @else
-							-->
-						
-						@guest
-						
-						@else
                         <li class="nav-item">
                             <a href="/admin/dashboard" class="nav-link">Manage Admin</a>
                         </li>
-						@if (Route::has('register'))
-							<li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-							</li>
-						@endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
