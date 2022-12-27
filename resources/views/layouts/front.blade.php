d81711765323934a6b506d4e2c5bef7a4288b83a
capt3n <tangkasfx@gmail.com>
2022-12-27 17:31:23 +0700

    top menu akses type user

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -55,9 +55,11 @@
                         <li class="nav-item">
                             <a href="/admin/dashboard" class="nav-link">Manage Admin</a>
                         </li>
-						<li class="nav-item">
+						@if (Route::has('register'))
+							<li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
+							</li>
+						@endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
