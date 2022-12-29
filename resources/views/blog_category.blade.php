aa45071bdfe4accd7b57b7c713445877c73cfc7d
capt3n <tangkasfx@gmail.com>
2022-12-29 16:59:31 +0700

    cloud image ke post

--- resources/views/blog_category.blade.php
+++ resources/views/blog_category.blade.php
@@ -75,7 +75,7 @@
                         <div class="box-post">
                             <div class="row">
                                 <div class="col-4">
-                                    <img src="{{$post->getPhoto()}}" alt="">
+                                    <img src="{{$post->getPhotoUrl()}}" alt="">
                                 </div>
                                 <div class="col-8 p-2">
                                     <div class="body">
